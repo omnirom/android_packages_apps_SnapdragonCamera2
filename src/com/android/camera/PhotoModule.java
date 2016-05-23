990e585d89ab8f396e902e9ef461bb246081cd81
maxwen <max.weninger@gmail.com>
2016-05-24 00:17:26 +0200

    SnapdragonCamera: add storage path switch for gallery app
    like https://github.com/omnirom/android_packages_apps_Camera2/commit/36cfe8c6a75046495318865443fdae568edcd97c
    
    Change-Id: I38b579bfee057faa4e245a386eb037d65da63710

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -4900,6 +4900,11 @@ public class PhotoModule
                     mPreferences.getString(CameraSettings.KEY_CAMERA_SAVEPATH, "0").equals("1"));
             mActivity.updateStorageSpaceAndHint();
             updateRemainingPhotos();
+
+            // Update the gallery app
+            Intent intent = new Intent("com.android.gallery3d.STORAGE_CHANGE");
+            intent.putExtra("pref_camera_storage_key", Storage.getRoot());
+            mActivity.sendBroadcast(intent);
         }
 
         if (CameraSettings.KEY_QC_CHROMA_FLASH.equals(pref.getKey())) {
--- src/com/android/camera/SDCard.java
+++ src/com/android/camera/SDCard.java
@@ -57,6 +57,13 @@ public class SDCard {
         return false;
     }
 
+    public String getRoot() {
+        if (mVolume == null) {
+            return null;
+        }
+        return mVolume.getPath();
+    }
+
     public String getDirectory() {
         if (mVolume == null) {
             return null;

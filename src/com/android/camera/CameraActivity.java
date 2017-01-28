5eca22deff8c8e86a8d6db33f2836951e4172412
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-01-28 14:56:35 +0100

    Snap: detect and use Camera2 if available
    Also add overlay option to enable support for Camera2 to retain current behaviour.
    
    Change-Id: I20939e33f4bb687e4abea11bbcdb9bf246b156e4

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -191,11 +191,15 @@ public class CameraActivity extends Activity
     private static boolean PIE_MENU_ENABLED = false;
     private boolean mDeveloperMenuEnabled = false;
 
+    private boolean mCamera2supported = false;
+    private boolean mCamera2enabled = false;
+
     /** This data adapter is used by FilmStripView. */
     private LocalDataAdapter mDataAdapter;
     /** This data adapter represents the real local camera data. */
     private LocalDataAdapter mWrappedDataAdapter;
 
+    private Context mContext;
     private PanoramaStitchingManager mPanoramaManager;
     private PlaceholderManager mPlaceholderManager;
     private int mCurrentModuleIndex;
@@ -1464,6 +1468,9 @@ public class CameraActivity extends Activity
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
+
+        mContext = getApplicationContext();
+
         // Check if this is in the secure camera mode.
         Intent intent = getIntent();
         String action = intent.getAction();
@@ -1541,9 +1548,14 @@ public class CameraActivity extends Activity
             }
         }
 
-        boolean cam2on = PersistUtil.getCamera2Mode();
-        if (cam2on && (moduleIndex == ModuleSwitcher.PHOTO_MODULE_INDEX ||
-                moduleIndex == ModuleSwitcher.VIDEO_MODULE_INDEX))
+        // Check if the device supports Camera API 2
+        mCamera2supported = CameraUtil.isCamera2Supported(mContext);
+        Log.d(TAG, "Camera API 2 supported: " + mCamera2supported);
+
+        mCamera2enabled = mCamera2supported && mContext.getResources().getBoolean(R.bool.support_camera_api_v2);
+        Log.d(TAG, "Camera API 2 enabled: " + mCamera2enabled);
+
+        if (mCamera2enabled && moduleIndex == ModuleSwitcher.PHOTO_MODULE_INDEX)
             moduleIndex = ModuleSwitcher.CAPTURE_MODULE_INDEX;
 
         mOrientationListener = new MyOrientationEventListener(this);
@@ -2065,10 +2077,9 @@ public class CameraActivity extends Activity
     }
 
     @Override
-    public void onModuleSelected(int moduleIndex) {
-        boolean cam2on = PersistUtil.getCamera2Mode();
+     public void onModuleSelected(int moduleIndex) {
         mForceReleaseCamera = moduleIndex == ModuleSwitcher.CAPTURE_MODULE_INDEX ||
-                (cam2on && moduleIndex == ModuleSwitcher.PHOTO_MODULE_INDEX);
+                (mCamera2enabled && moduleIndex == ModuleSwitcher.PHOTO_MODULE_INDEX);
         if (mForceReleaseCamera) {
             moduleIndex = ModuleSwitcher.CAPTURE_MODULE_INDEX;
         }
